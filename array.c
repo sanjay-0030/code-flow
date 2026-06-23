2f9782718fe2f4a51b6193ba1cec354ace1eeaad
sanjay <sanjaysanjayr2006@gmail.com>
2026-06-23 21:28:45 +0530

    Fix : for loop change

--- array.c
+++ array.c
@@ -3,7 +3,7 @@ int main(){
     int number[] ={1,2,3,4,5,};
     int i;
     printf("Array element are:\n");
-    for(i=1;i<5;i++){
+    for(i=3;i<5;i++){
         printf("%d\n",number[i]);
     }
     return 0;
