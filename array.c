08875e85803e8dfd728c009263dd436cfdb23fcb
sanjay-0030 <sanjaysanjayr2006@gmail.com>
2026-06-23 21:27:14 +0530

    array.c

---
+++ array.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int number[] ={1,2,3,4,5,};
+    int i;
+    printf("Array element are:\n");
+    for(i=1;i<5;i++){
+        printf("%d\n",number[i]);
+    }
+    return 0;
+}
